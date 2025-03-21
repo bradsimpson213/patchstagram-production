fcca192fbdb6d0d502b504636f49d08d6b420b2f
Brad Simpson <bradsimpson@icloud.com>
2025-03-21 09:24:09 -0400

    working on routes and new models

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,15 +1,31 @@
 from flask import Blueprint, requests
-
 from flask_login import login_required, current_user
-
-
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+import os
 
 image_routes = Blueprint('images', __name__)
 
 
 @image_routes.route('/generate_tags',methods=["Posts"])
 def get_image_tags():
-    pass
+    
+    client = ImageAnalysisClient(
+         endpoint=os.environ.get('VISION_ENDPOINT'),
+         credential=AzureKeyCredential(os.environ.get('VISION_KEY'))
+    )
+
+    result = client.analyze_from_url(
+         image_url="https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png",
+         visual_features=[VisualFeatures.CAPTION, VisualFeatures.READ],
+         gender_neutral_caption=True,  # Optional (default is False)
+    )
+
+    if result.tags is not None:
+        print(" Tags:")
+        for tag in result.tags.list:
+            print(f"   '{tag.name}', Confidence {tag.confidence:.4f}")
     # form = PostForm()
     
     # form["csrf_token"].data = request.cookies["csrf_token"]
@@ -19,16 +35,6 @@ def get_image_tags():
     # if form.validate_on_submit():
 
 
-from azure.ai.vision.imageanalysis import ImageAnalysisClient
-from azure.ai.vision.imageanalysis.models import VisualFeatures
-from azure.core.credentials import AzureKeyCredential
 
-# Set the values of your computer vision endpoint and computer vision key
-# as environment variables:
-try:
-    endpoint = os.environ["VISION_ENDPOINT"]
-    key = os.environ["VISION_KEY"]
-except KeyError:
-    print("Missing environment variable 'VISION_ENDPOINT' or 'VISION_KEY'")
-    print("Set them before running this sample.")
-    exit()
+
+
